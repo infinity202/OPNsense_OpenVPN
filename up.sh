3ab5682afc1ed6a90319297b3be42484aaffc0af
infinity202 <24571949+infinity202@users.noreply.github.com>
2021-01-04 21:47:11 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
-## this file just call a Python3 script. Should be possible to intergrate it into 1 file, but i don't know how ;-) 
-## file location : /var/etc
-
 #!/bin/sh
 /usr/local/bin/python3 /var/etc/client-up-extra.py
+
+## this file just call a Python3 script. Should be possible to intergrate it into 1 file, but i don't know how ;-) 
+## file location : /var/etc
 
