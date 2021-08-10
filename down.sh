7e9835f7d4355d577c72b9031b33d8c7cea29fc5
infinity202 <24571949+infinity202@users.noreply.github.com>
2021-08-10 11:29:07 +0200

    Create down.sh

---
+++ down.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+/usr/local/bin/python3 /var/etc/client-down-extra.py
+
+## this file just calls a Python3 script. Should be possible to intergrate it into 1 file, but i don't know how ;-)
+## file location : /var/etc
+## wil be used to email when OpenVPN client disconnects
